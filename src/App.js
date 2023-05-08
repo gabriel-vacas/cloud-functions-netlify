0350a289aaaeeda803394551d22cfd54b497f88a
gabriel-vacas <gva1100@hotmail.com>
2023-05-08 01:24:01 -0500

    Add API

--- src/App.js
+++ src/App.js
@@ -1,25 +1,19 @@
-import logo from './logo.svg';
-import './App.css';
-
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  const testAzureFunction = async () => {
+    const reCaptchaData = {
+      secret: 'token',
+      response: 'token',
+    };
+    const response = await window
+      .fetch("/api/test", {
+        method: `POST`,
+        body: JSON.stringify(reCaptchaData),
+      })
+      .then((res) => res.json());
+    console.log("RES", response);
+  };
+
+  return <button onClick={() => testAzureFunction()}>Test</button>;
 }
 
 export default App;
